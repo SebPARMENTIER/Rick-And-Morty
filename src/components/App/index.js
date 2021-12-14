5abdfabe25d1b0c885401fbe264f029bacf846af
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-12-14 16:51:18 +0100

    Create components and custom Header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,33 @@
 // == Import npm
 import React from 'react';
+import { Routes, Route } from 'react-router';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './app.scss';
+import Header from 'src/components/Header';
+import Home from 'src/components/Home';
+import Character from 'src/components/Character';
+import Episode from 'src/components/Episode';
+import Location from 'src/components/Location';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Container fluid="true" className="h-100">
+      <Row>
+        <Col className="col-sm-auto col-md-auto col-lg-auto">
+          <Header />
+          {/* <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/character" element={<Character />} />
+            <Route exact path="/episode" element={<Episode />} />
+            <Route exact path="/location" element={<Location />} />
+          </Routes> */}
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,47 @@
+// == Import npm
+import React from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+// == Import local
+import './header.scss';
+import logo from 'src/assets/Rick_and_Morty_logo.png';
+
+// == Component
+const Header = () => (
+  <Navbar fixed="top" collapseOnSelect expand="lg">
+    <Container>
+      <Navbar.Brand className="header-home-link" href="/">
+        <img className="header-logo" src={logo} alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link
+            className="header-home-link"
+            style={{ color: '#62a4ab' }}
+            href="/character"
+          >
+            Personnages
+          </Nav.Link>
+          <Nav.Link
+            className="header-home-link"
+            style={{ color: '#62a4ab' }}
+            href="/episode"
+          >
+            Episodes
+          </Nav.Link>
+          <Nav.Link
+            className="header-home-link"
+            style={{ color: '#62a4ab' }}
+            href="/location"
+          >
+            Lieux
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+// == Export
+export default Header;
