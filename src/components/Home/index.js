5e00279db925afea7d0e8af28cc46175cc72a1a9
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-12-16 11:36:58 +0100

    Add Rick and Morty picture on Home component and link to Game

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,23 @@
 // == Import npm
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 
 // == Import local
 import './home.scss';
+import morty from 'src/assets/morty.png';
+import rick from 'src/assets/rick.png';
 
 // == Component
 const Home = () => (
   <div className="home">
     <Row>
       <Col className="col-sm-auto col-md-auto col-lg-auto">
+        <div className="home-container">
+        <div className="home-img-r">
+          <img className="home-img-rick" src={rick} alt="Rick" />
+        </div>
+        <Link to="/character" className="game">
         <div className="portal">
           <div className="swish">
             <span></span>
@@ -43,6 +51,12 @@ const Home = () => (
             </div>
           </div>
         </div>
+        </Link>
+        <div className="home-img-m">
+          <img className="home-img-morty" src={morty} alt="Morty" />
+        </div>
+        </div>
+        
       </Col>
     </Row>
   </div>
